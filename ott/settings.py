31b649bc7c6adf2d56923d98024e1b6d9c4e6911
Sainath Tellapuram <sainath2381@gmail.com>
2021-08-19 23:07:18 +0530

    added link

--- ott/settings.py
+++ ott/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6_94t%#c10-d4qjhxdk426)1+vs5b008&(a3_c!f*nb!8wj18d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ytasott.herokuapp.com']
 
 
 # Application definition
